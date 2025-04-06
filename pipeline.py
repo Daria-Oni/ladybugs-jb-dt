ac48faa109916853bc4adfe4c9529dfe171bf406
besteguney <63497613+besteguney@users.noreply.github.com>
2025-04-06 07:42:43 +0200

    Added the latest pipeline.

--- pipeline.py
+++ pipeline.py
@@ -2,23 +2,26 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 import ast
+import json
+import re
+from nltk import pos_tag, word_tokenize
+from nltk.corpus import stopwords
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from modules.matching import InformationChecker
-from modules.embedding import text_columns_to_vector
+from modules.embedding import text_columns_to_vector, load_glove_vectors, get_glove_embedding
 from modules.parser import unzip_files, create_csvs
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from modules.model import HybridModel
 from modules.preprocessing import classify_duration, calculate_education_level, calculate_year_diff
 from sklearn.metrics import accuracy_score
+from sentence_transformers import SentenceTransformer
+# from sklearn.feature_extraction.text import TfidfVectorizer
 
 # If not parsed parse the zip files
 # unzip_files()
 # create_csvs()
-
-# # Creating the embedding for the descriptions
-# text_fields = ["Summary Note", "Family Background", "Education Background", "Occupation History", "Wealth Summary", "Client Summary"]
-# #text_columns_to_vector(df_checked, text_fields)
 
 # Load the data
 df_account = pd.read_csv("account.csv")
@@ -26,16 +29,35 @@ df_profile = pd.read_csv("profile.csv")
 df_description = pd.read_csv("description.csv")
 df_passport = pd.read_csv("passport.csv")
 df_labels = pd.read_csv("labels.csv")  
-df_embeddings = pd.read_csv("description_embedded.csv")
+df_embeddings = pd.read_csv("desc_emb.csv")
 
-
-# Join the data from csv's
+# # Join the data from csv's
 df = pd.concat([df_account, df_profile, df_description, df_passport, df_labels, df_embeddings], axis=1)
 df = df.loc[:, ~df.columns.duplicated()]
 
-# Fixing some features
+categorical_col = ['gender', 'marital_status', 'investment_horizon', 'investment_experience', 'type_of_mandate']
+feature_cols = ['Age', 'education_level',  'employment_duration', 'Total_Assets_scaled', 'has_switzerland']
+
+
 df['investment_horizon'] = df['investment_horizon'].apply(classify_duration)
+
+
 df['Total_Assets'] = df['aum_savings'] + df['aum_inheritance'] + df['aum_real_estate_value']
+df['Total_Assets'] = df.apply(
+    lambda row: row['Total_Assets'] * 1.06 if row['currency'] == 'CHF' 
+    # else row['Total_Assets'] * 0.13 if row['currency'] == 'DKK' 
+    else row['Total_Assets'],
+    axis=1
+)
+scaler = StandardScaler()
+# Fit and transform the numeric column (feature1) and add it to the dataframe
+df['Total_Assets_scaled'] = scaler.fit_transform(df[['Total_Assets']])
+
+def parse_country_list(country_list_str):
+    return ast.literal_eval(country_list_str)
+
+# Apply the function to parse the 'preferred_markets' column
+df['preferred_markets'] = df['preferred_markets'].apply(parse_country_list)
 df['Age'] = pd.to_datetime('today').year - pd.to_datetime(df['birth_date']).dt.year
 
 df['education_level'] = df['higher_education'].apply(lambda x: ast.literal_eval(x))
@@ -43,37 +65,10 @@ df['education_level'] = df['education_level'].apply(len)
 
 df['employment_history'] = df['employment_history'].apply(lambda x: ast.literal_eval(x))
 df['employment_duration'] = df['employment_history'].apply(calculate_year_diff)
+df['has_switzerland'] = df['preferred_markets'].apply(lambda x: 1 if 'Switzerland' in x else 0)
 
-
-# Feature Selection
-print(df.columns)
-categorical_col = ['gender', 'marital_status', 'investment_horizon', 'investment_experience', 'type_of_mandate']
-feature_cols = ['Age', 'education_level',  'employment_duration', 'Total_Assets']
 df = df[feature_cols+categorical_col+['client_id', 'label']]
 df = pd.get_dummies(df, columns=categorical_col)
-
-# print(df.columns)
-
-# # embedded_columns = ['embedded_Summary Note',
-# #        'embedded_Family Background', 'embedded_Education Background',
-# #        'embedded_Occupation History', 'embedded_Wealth Summary',
-# #        'embedded_Client Summary']
-
-# # for col in embedded_columns:
-# #     df[col] = df[col].apply(lambda x: x.strip())
-#     # df[col] = df[col].apply(lambda x: x.replace(' ', ','))
-    
-#     # df[col] = df[col].apply(lambda x: np.array(ast.literal_eval(x)))
-
-# # print(type(df.loc[0,'embedded_Client Summary']))
-# # print(df.loc[0,'embedded_Client Summary'])
-
-# # X = np.vstack(
-# #     df.apply(lambda row: np.hstack([row[embedded_columns].values, row[feature_cols].values]), axis=1)
-# # )
-
-# # print(X.shape)  # Should be (num_rows, len(embedding) + len(other_cols))
-# # print(X[0])
 
 train_df, test_df = train_test_split(df, test_size=0.1, random_state=42)
 
@@ -95,14 +90,14 @@ model.fit(X_train.values, y_train.values)
 predictions = pd.DataFrame()
 
 for index, row in test_df.iterrows():
+    predictions.loc[index, 'client_id'] = f'client_{row['client_id']}'
     if row['client_id'] in rejections:
         predictions.loc[index, 'label'] = 0
     else:
         predict = model.predict_proba(row[feature_cols].values.reshape(1, -1))[:, 1]
-        # print(predict.shape)
-        # print('****', predict)
         predictions.loc[index, 'label'] = (predict[0] > 0.5).astype(int)
 
+predictions.to_csv('solution2.csv', header=False, index=False, sep=';')
 score = accuracy_score(test_df['label'], predictions)
 print(score)
 
@@ -115,7 +110,9 @@ print(score)
 #     if row['client_id'] in rejections:
 #         predictions.loc[index, 'label'] = 0
 #     else:
-#         predict = model.predict(row[feature_cols].values.reshape(1, -1))
-#         predictions.loc[index, 'label'] = predict[0]
+#         predict = model.predict_proba(row[feature_cols].values.reshape(1, -1))[:, 1]
+#         predictions.loc[index, 'label'] = (predict[0] > 0.5).astype(int)
 
 # print(predictions.shape)
+# score = accuracy_score(test_df['label'], predictions)
+# print(score)
